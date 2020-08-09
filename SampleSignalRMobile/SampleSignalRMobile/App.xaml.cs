180d03a55cbfda771a0fe71eb3de02e74598e335
puthik Ma <puthik.ma@gmail.com>
2020-08-09 16:55:34 -0500

    Add project files.

---
+++ SampleSignalRMobile/SampleSignalRMobile/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SampleSignalRMobile.Services;
+using SampleSignalRMobile.Views;
+using Xamarin.Essentials;
+
+namespace SampleSignalRMobile
+{
+    public partial class App : Application
+    {
+
+        public static string AzureBackendUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5001" : "http://localhost:5001";
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SampleSignalRMobile/SampleSignalRMobile/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using SampleSignalRMobile.Models;
+using SampleSignalRMobile.Views;
+using Microsoft.AspNetCore.SignalR.Client;
+using Xamarin.Essentials;
+
+namespace SampleSignalRMobile.ViewModels
+{
+    public class ItemsViewModel : BaseViewModel
+    {
+        public ObservableCollection<Item> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+        HubConnection hubConnection;
+        bool connected;
+        public ItemsViewModel()
+        {
+            Title = "Browse";
+            Items = new ObservableCollection<Item>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            {
+                var newItem = item as Item;
+                Items.Add(newItem);
+                await DataStore.AddItemAsync(newItem);
+            });
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl($"{App.AzureBackendUrl}/hubs/messages").Build();
+            hubConnection.On<string>("NewItem", (item) =>
+            {
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    try
+                    {
+                        var newItem = new Item();
+                        newItem.Description = item;
+                        Items.Insert(0, newItem);
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                });
+            });
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+       
+
+            IsBusy = true;
+
+            try
+            {
+                if (!connected)
+                    await hubConnection.StartAsync();
+
+                connected = true;
+                await App.Current.MainPage.DisplayAlert("", "Connected", "OK");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            try
+            {
+               
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
